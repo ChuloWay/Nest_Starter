7ab66b3ffd9890b6f54b46e2195f4ee747a464da
Okoye Victor <okoyeebuka64@gmail.com>
2022-08-12 16:26:13 +0100

    typeorm and sqlite

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { BookModule } from './books/book.module';
+import { ContactsModule } from './contacts/contacts.module';
 
 @Module({
-  imports: [BookModule],
+  imports: [BookModule, ContactsModule],
   controllers: [AppController],
   providers: [AppService],
 })
